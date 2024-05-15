c2c149ac542ec659c0cac9a0203793c44057964f
Lior Ruslander <liorruslander@gmail.com>
2024-05-16 00:56:23 +0300

    add upload to s3 with temp file, add *.tmp.json to .gitignore

--- main.py
+++ main.py
@@ -2,15 +2,18 @@
 import boto3
 import os
 import logging
+import tempfile
 from botocore.exceptions import ClientError
+import json
+import contextlib
 
 server_token="notsecured"   # todo: change it to get from env
 
 sqs_client = boto3.client("sqs",
     region_name="us-east-1",#os.environ.get('AWS_DEFAULT_REGION'),
     endpoint_url="http://localhost:4566",#os.environ.get('AWS_ENDPOINT'),
-    aws_access_key_id="my_access_key", #os.environ.get('AWS_ACCESS_KEY_ID'),
-    aws_secret_access_key="my_secret_key")#os.environ.get('AWS_SECRET_ACCESS_KEY'))
+    aws_access_key_id="test", #os.environ.get('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key="test")#os.environ.get('AWS_SECRET_ACCESS_KEY'))
 queue_url = sqs_client.get_queue_url(
         QueueName="checkpoint-SQS",
     )["QueueUrl"]
@@ -23,22 +26,34 @@ s3_client = boto3.client("s3",
 
 bucket_name="checkpoint-s3"
 
-def read_message_from_sqs():
+def read_messages_from_sqs():
     response = sqs_client.receive_message(
         QueueUrl=queue_url,
         MaxNumberOfMessages=2
     )
+    #print(response)
+    print(response["Messages"])
+    for message in response["Messages"]:
+        manage_message(message)
 
+def manage_message(message):
+    upload_message_to_s3(message)
+    delete_message_from_sqs(message)
 
-
-
-    
-def upload_message_to_s3(message):
+def delete_message_from_sqs(message):
     pass
+   
+def upload_message_to_s3(message):
+    tfile=tempfile.NamedTemporaryFile(mode='w',dir=os.getcwd(), suffix=".tmp.json", delete=False)
+    tf_name=tfile.name
+    json.dump(json.loads(message["Body"]), tfile)
+    tfile.close()
+    upload_file_to_s3(file_name=tfile.name, bucket=bucket_name, object_name=message["MD5OfBody"])
+    with contextlib.suppress(FileNotFoundError):
+        os.remove(tf_name)
 
 
-
-def upload_file_tos3(file_name, bucket, object_name=None):
+def upload_file_to_s3(file_name, bucket, object_name=None):
     """Upload a file to an S3 bucket
 
     :param file_name: File to upload
@@ -65,4 +80,4 @@ def upload_file_tos3(file_name, bucket, object_name=None):
         
 
 if __name__ == '__main__':
-    read_message_from_sqs()
+    read_messages_from_sqs()
